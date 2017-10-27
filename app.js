68904f2a93ea492ec877b15092521de7fd19b005
Shahbaz Khan <shahbaz.khan1003@gmail.com>
2017-10-28 01:18:57 +0530

    added dialog

--- app.js
+++ app.js
@@ -17,6 +17,29 @@ var connector = new builder.ChatConnector({
 server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-   session.send("You said: %s", session.message.text);
-});
+// var bot = new builder.UniversalBot(connector, function (session) {
+//    session.send("You said: %s", session.message.text);
+// });
+
+
+var bot = new builder.UniversalBot(connector, [
+  function (session) {
+      session.send("Welcome to the dinner reservation.");
+      builder.Prompts.time(session, "Please provide a reservation date and time (e.g.: June 6th at 5pm)");
+  },
+  function (session, results) {
+      session.dialogData.reservationDate = builder.EntityRecognizer.resolveTime([results.response]);
+      builder.Prompts.text(session, "How many people are in your party?");
+  },
+  function (session, results) {
+      session.dialogData.partySize = results.response;
+      builder.Prompts.text(session, "Who's name will this reservation be under?");
+  },
+  function (session, results) {
+      session.dialogData.reservationName = results.response;
+
+      // Process request and display reservation details
+      session.send(`Reservation confirmed. Reservation details: <br/>Date/Time: ${session.dialogData.reservationDate} <br/>Party size: ${session.dialogData.partySize} <br/>Reservation name: ${session.dialogData.reservationName}`);
+      session.endDialog();
+  }
+]);
