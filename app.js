725fb671f6b340aff69255306558328a9fc8dec1
Shahbaz Khan <shahbaz.khan1003@gmail.com>
2017-10-30 15:30:14 +0530

    added voice

--- app.js
+++ app.js
@@ -15,13 +15,23 @@ var connector = new builder.ChatConnector({
 });
 
 // Listen for messages from users 
+
+var savedAddress;
+
 server.post('/api/messages', connector.listen());
+
+// Do GET this endpoint to delivey a notification
+server.get('/api/CustomWebApi', (req, res, next) => {
+  sendProactiveMessage(savedAddress);
+  res.send('triggered');
+  next();
+}
+);
 
 
 
 var bot = new builder.UniversalBot(connector, [
   function (session) {
-      createSigninCard(session);
       session.send("Sure! I can book that for you");
       builder.Prompts.time(session, "What time do want it to be booked?");
   },
@@ -37,14 +47,33 @@ var bot = new builder.UniversalBot(connector, [
           `Reservation confirmed.<br/>${session.dialogData.reservationName} 
           booked a cab for ${moment(session.dialogData.reservationDate).format('DD MMM H:mm')}.`
         );
-
       session.endDialog();
   }
 ]);
 
 
-function createSigninCard(session) {
-  return new builder.SigninCard(session)
-      .text('BotFramework Sign-in Card')
-      .button('Sign-in', 'https://login.microsoftonline.com')
+// root dialog
+bot.dialog('/', function(session, args) {
+  
+    savedAddress = session.message.address;
+  
+    var message = 'Hello! In a few seconds I\'ll send you a message proactively to demonstrate how bots can initiate messages.';
+    session.send(message);
+    
+    message = 'You can also make me send a message by accessing: ';
+    message += 'http://localhost:' + server.address().port + '/api/CustomWebApi';
+    session.send(message);
+  
+    setTimeout(() => {
+     sendProactiveMessage(savedAddress);
+    }, 5000);
+  });
+
+
+// send simple notification
+function sendProactiveMessage(address) {
+  var msg = new builder.Message().address(address);
+  msg.text('Hello, this is a notification');
+  msg.textLocale('en-US');
+  bot.send(msg);
 }
